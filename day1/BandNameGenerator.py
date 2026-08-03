f95d4733be2f12d8015ef5b20ab4298fee244f76
mrsam468 <ibenjaminsamuel47@gmail.com>
2026-08-03 14:46:14 +0100

    day1 project completed

---
+++ day1/BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("welcome to the band name Generator")
+name_of_community = input("What is the name of the community you lived in? ")
+pet_name = input("what is the name of your favourite pet ")
+print("your band name could be " + name_of_community +" "+ pet_name)
